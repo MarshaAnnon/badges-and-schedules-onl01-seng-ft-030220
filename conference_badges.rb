f67a44425a78792faaaa56d8fc96234fd9c7f7e8
Marsha Annon <marshaannon@gmail.com>
2020-03-05 12:17:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   return("Hello, my name is #{name}.")
 end
+
+def batch_badge_creator(attendees)
+  badge_messages=[]
+  attendees.each do |name| badge_messages << "Hello, my name is #{name}."
+  end
+end
