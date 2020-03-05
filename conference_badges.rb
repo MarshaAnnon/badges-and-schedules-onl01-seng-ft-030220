e0f12013adb31eda0804e14f07e16c0778f9f2c7
Marsha Annon <marshaannon@gmail.com>
2020-03-05 13:11:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(name)
   rooms=[]
   room = 1
-  name.each_with_index(1) do |name, room_assignment|
+  name.each_with_index(0) do |name, room_assignment|
     rooms << room_assignments = "Hello, #{name}! You'll be assigned to room #{room_assignments}!"
     room+=1
 end
