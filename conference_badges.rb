cafe408bb67a71c691d9b5f0841bc72749951a77
Marsha Annon <marshaannon@gmail.com>
2020-03-05 13:44:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,12 +3,10 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  new_array=[]
-  attendees.each do |name| new_array << "Hello, my name is #{name}."
+  new_array = []
+  array.each{|name| new_array << "Hello, my name is #{name}."}
   new_array
   end
-end
-
 
 def assign_rooms(name)
   rooms = []
