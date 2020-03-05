681aceba18a30cb8d5c7afc1bd99e83a3854e336
Marsha Annon <marshaannon@gmail.com>
2020-03-05 13:00:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,10 @@ def batch_badge_creator(array)
   new_array
 end
 
-def assign_rooms(speakers)
+def assign_rooms(name)
   rooms=[]
-  speakers.each_with_index {|rooms, index| rooms.push("Hello, #{name}! You'll be assigned to room #{room}!") }
-  rooms+=1
+  name.each_with_index(1) {|name, room_assignment|
+    rooms << room_assignments = "Hello, #{name}! You'll be assigned to room #{room_assignments}!" }
 end
   rooms
 end
