f8ae47b26ffe1af19a6c06b0c88a149b126bea47
Marsha Annon <marshaannon@gmail.com>
2020-03-05 13:42:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,3 +19,12 @@ def assign_rooms(name)
 end
   new_array
 end
+
+def printer(name)
+  batch_badge_creator(name).each do |badges|
+    puts badges
+  end
+  assign_rooms(name).each do |room_assignment|
+    puts room_assignment
+  end
+end
